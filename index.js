63207e38ef5d806bab1c73e1746bf242085856d1
viplav <viplavg1999@gmail.com>
2020-09-08 19:21:53 +0530

    First commit and my intro to nodejs

---
+++ index.js
@@ -0,0 +1,7 @@
+//jshint esversion:6
+
+var supervillains = require("supervillains");
+var superheroes = require("superheroes");
+
+console.log(superheroes.random());
+console.log(supervillains.random());
